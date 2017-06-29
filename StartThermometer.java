9d42c7103271a538fd3a4b17773098cec8a6ff81
edroberts1943 <ed.roberts@verizon.net>
2017-06-28 22:06:58 -0400

    Add files via upload

---
+++ StartThermometer.java
@@ -0,0 +1,35 @@
+
+/**
+ * StartThermometer
+ * 
+ * @author (Edward B Roberts) 
+ * @version (6-28-17)
+ */
+
+
+
+public class StartThermometer
+{  
+static RemoteThermometer thisThermometer;
+static int unit;
+    /**
+     * Constructor for objects of class StartThermostat
+     */
+    public void StartThermometer()
+    {
+ 
+    }
+
+    /**
+     * main()      Program to initiate operation of a remote thermostat
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public static void main (String[] args) {
+    {
+        thisThermometer = new RemoteThermometer();
+    }
+   
+   }
+}
